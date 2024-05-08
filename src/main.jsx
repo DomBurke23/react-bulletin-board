f6f19b97b11894730097cd4e6aa5faf1f0176487
dsmith-FootballPools <dominique.smith@footballpools.com>
2024-05-08 11:21:41 +0100

    1st custom component

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// The code here is executed first when this website is being loaded into the browser.
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
