c35f32dca161bef8bd65138e8e5fe63a7b2a86b7
dsmith-FootballPools <dominique.smith@footballpools.com>
2024-05-09 14:13:36 +0100

    output list data

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -24,12 +24,23 @@ function PostsList({ isVisible, onStopPosting }) {
   }
 
   // TODO replace and dynamically render posts here
+  // key should be a unique id, for this example body is fine
   return (
     <>
       {modalContent}
-      <ul className={classes.posts}>
-        <Post author="Dom" body="Intro to react." />
-      </ul>
+      {posts.length > 0 && (
+        <ul className={classes.posts}>
+          {posts.map((post) => (
+            <Post key={post.body} author={post.author} body={post.body} />
+          ))}
+        </ul>
+      )}
+      {posts.length === 0 && (
+        <div style={{ textAlign: "center", color: "white" }}>
+          <h2>There are no posts yet.</h2>
+          <p> Start adding some! </p>
+        </div>
+      )}
     </>
   );
 }
