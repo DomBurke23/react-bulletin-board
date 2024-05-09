47ad36473fd6e8fef2412295b39a6da26d615978
dsmith-FootballPools <dominique.smith@footballpools.com>
2024-05-09 09:40:55 +0100

    state feature in action

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,14 +1,23 @@
+// functions that start with use in react are considered hooks.
+// hooks must be executed inside a component function
+// hooks can not be executed in regular javascript functions
+// hooks typically change something about that component or how react behaves with that component.
+import { useState } from "react";
 import classes from "./NewPost.module.css";
 
 function NewPost() {
+  // register state
+  const [enteredBody, setEnteredBody] = useState("");
+
   // vanilla javascript way of handling updating - imperative approach
   // tells browser that we want to get hold of text area and add an event listener followed by what should happen
   //document.querySelector('textarea').addEventListener('change', function(){});
   // react we use declarative approach eg onKeyDown or onChange
 
   function changeBodyHandler(event) {
-    // prints each letter out eg h he hel ..
+    // prints each letter (keystroke) out eg h he hel ..
     console.log(event.target.value);
+    setEnteredBody(event.target.value);
   }
 
   return (
@@ -23,6 +32,7 @@ function NewPost() {
           onChange={changeBodyHandler}
         />
       </p>
+      <p>{enteredBody}</p>
       <p>
         <label htmlFor="name">Your name</label>
         <input type="text" id="name" required />
