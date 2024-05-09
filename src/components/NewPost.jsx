a0c9f334d84c27ce7043f1f328f72775c51c03d4
dsmith-FootballPools <dominique.smith@footballpools.com>
2024-05-09 09:53:29 +0100

    lifting state up

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,25 +1,6 @@
-// functions that start with use in react are considered hooks.
-// hooks must be executed inside a component function
-// hooks can not be executed in regular javascript functions
-// hooks typically change something about that component or how react behaves with that component.
-import { useState } from "react";
 import classes from "./NewPost.module.css";
 
-function NewPost() {
-  // register state
-  const [enteredBody, setEnteredBody] = useState("");
-
-  // vanilla javascript way of handling updating - imperative approach
-  // tells browser that we want to get hold of text area and add an event listener followed by what should happen
-  //document.querySelector('textarea').addEventListener('change', function(){});
-  // react we use declarative approach eg onKeyDown or onChange
-
-  function changeBodyHandler(event) {
-    // prints each letter (keystroke) out eg h he hel ..
-    console.log(event.target.value);
-    setEnteredBody(event.target.value);
-  }
-
+function NewPost(props) {
   return (
     <form className={classes.form}>
       <p>
@@ -29,13 +10,12 @@ function NewPost() {
           required
           rows={3}
           // sets up an event listner
-          onChange={changeBodyHandler}
+          onChange={props.onBodyChange}
         />
       </p>
-      <p>{enteredBody}</p>
       <p>
         <label htmlFor="name">Your name</label>
-        <input type="text" id="name" required />
+        <input type="text" id="name" required onChange={props.onAuthorChange} />
       </p>
     </form>
   );
--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,17 +1,44 @@
+// functions that start with use in react are considered hooks.
+// hooks must be executed inside a component function
+// hooks can not be executed in regular javascript functions
+// hooks typically change something about that component or how react behaves with that component.
+import { useState } from "react";
 import Post from "./Post";
 import NewPost from "./NewPost";
-import classes from './PostsList.module.css';
+import classes from "./PostsList.module.css";
 
-function PostsList(){
-    return (
-        <>
-            <NewPost />
-            <ul className={classes.posts}>
-                <Post author="Dominique" body="React.js is awesome!" />
-                <Post author="Dom" body="Intro to react." />
-            </ul>
-        </>
-    );
+function PostsList() {
+  // register state
+  const [enteredBody, setEnteredBody] = useState("");
+  const [enteredAuthor, setEnteredAuthor] = useState("");
+
+  // vanilla javascript way of handling updating - imperative approach
+  // tells browser that we want to get hold of text area and add an event listener followed by what should happen
+  //document.querySelector('textarea').addEventListener('change', function(){});
+  // react we use declarative approach eg onKeyDown or onChange
+
+  function bodyChangeHandler(event) {
+    // prints each letter (keystroke) out eg h he hel ..
+    console.log(event.target.value);
+    setEnteredBody(event.target.value);
+  }
+
+  function authorChangeHandler(event) {
+    setEnteredAuthor(event.target.value);
+  }
+
+  return (
+    <>
+      <NewPost
+        onBodyChange={bodyChangeHandler}
+        onAuthorChange={authorChangeHandler}
+      />
+      <ul className={classes.posts}>
+        <Post author={enteredAuthor} body={enteredBody} />
+        <Post author="Dom" body="Intro to react." />
+      </ul>
+    </>
+  );
 }
 
 export default PostsList;
